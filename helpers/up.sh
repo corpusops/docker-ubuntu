a3261f4c5e399c27b8c820dcc8878565483c76d1
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2023-11-03 22:25:32 +0000

    refresh

--- helpers/up.sh
+++ helpers/up.sh
@@ -48,6 +48,9 @@ elif [ -e /etc/redhat-release ];then
     DISTRIB_RELEASE=$(echo $(head  /etc/issue)|awk '{print tolower($3)}')
 fi
 DISTRIB_MAJOR="$(echo ${DISTRIB_RELEASE}|sed -re "s/\..*//g")"
+if [ "x${DISTRIB_ID}" = "xcentos" ] && ( echo  "${DISTRIB_MAJOR}" | grep -Eq "^(6|7|8)");then
+    sed -i 's/mirrorlist/#mirrorlist/g;s|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-*
+fi
 if ( grep -q amzn /etc/os-release );then
     yuminstall findutils
     if ( amazon-linux-extras help >/dev/null 2>&1 );then
@@ -201,6 +204,7 @@ if [ -e /etc/fedora-release ];then
 fi
 if ( echo "$DISTRIB_ID $DISTRIB_RELEASE $DISTRIB_CODENAME" | egrep -iq alpine );then
     log "Upgrading alpine"
+    apk update && apk add bash
     apk upgrade --update-cache --available
 fi
 ./bin/fix_letsencrypt.sh
@@ -214,6 +218,5 @@ if ! ( echo foo|envsubst >/dev/null 2>&1);then
         echo "envsubst is missing"
     fi
 fi
-find /etc/rsyslog.d -name "*.conf" -not -type d \
-    | while read f;do mv -vf "$f" "$f.sample";done
+find /etc/rsyslog.d -name "*.conf" -not -type d |while read f;do mv -vf "$f" "$f.sample";done
 # vim:set et sts=4 ts=4 tw=0:
